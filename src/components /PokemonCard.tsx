9c490e25d785eab3cbaacbcc923fe63beebfd303
Laury Lutinclear <laury.lutin@gmail.com>
2025-04-02 22:13:12 +0200

    Commit props.1

--- src/components /PokemonCard.tsx
+++ src/components /PokemonCard.tsx
@@ -1,32 +1,17 @@
 
+  
+  function PokemonCard({ pokemon }) {
 
-  
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  
-  function PokemonCard() {
-    const pokemon = pokemonList[1];
 return (
-
 <>
-      <p>{pokemon.name}</p>
+<p>{pokemon.name}</p>
       {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
+        <img src={pokemon.imgSrc} alt={pokemon.name} />) : ( <p>???</p>
+          )}
     </>
-
+    )
+  }
    
-      )}
     
 
   export default PokemonCard;
