4db0edb677d138c4b5bf060e91c2dd3e71dda075
Laury Lutin <laury.lutin@gmail.com>
2025-05-07 23:52:24 +0200

    create NavBar in components

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,36 @@ import { useState } from "react";
 import "./App.css";
 
 import PokemonCard from "./components /PokemonCard";
+import NavBar from "./components /NavBar";
+
 const pokemonList = [
   {
-    id:0,
-    
-    name:  "bulbasaur",
+    id: 0,
+
+    name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    id:1,
+    id: 1,
     name: "charmander",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
   {
-    id:2,
+    id: 2,
     name: "squirtle",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
   },
   {
-    id:3,
+    id: 3,
     name: "pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
-    id:4,
+    id: 4,
     name: "mew",
   },
 ];
@@ -45,24 +47,12 @@ function App() {
 
 
   return (
-    
+
     <div>
-      <PokemonCard pokemon={pokemon}  />
-    
-     <nav>
-				{pokemonList.map((pokemon) => (
-					<button
-						type="button"
-						key={pokemon.name}
-						onClick={() => setPokemonName(pokemon.name)}
-					>
-						{pokemon.name}
-					</button>
-				))}
-			</nav>
-	
-		</div>
-	);
+      <NavBar setPokemonName={setPokemonName} pokemonList={pokemonList} />
+      <PokemonCard pokemon={pokemon} />
+    </div>
+  );
 }
 
 export default App;
